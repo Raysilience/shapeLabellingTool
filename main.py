e9cb34942b4ad0f2924b8affba30cfaa5a12e682
ruizhang <rui27.zhang@tcl.com>
2021-09-08 15:19:19 +0800

    feat:CVPLATFORM-142 turn on debug mode

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     # # pts = FileUtil.csv_to_arr('./test/54.csv')
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
     whiteboard = Gameboard()
     # # whiteboard.set_points(pts)
     whiteboard.draw()
